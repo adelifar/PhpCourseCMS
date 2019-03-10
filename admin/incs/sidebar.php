e0cc93e73a0b4fd0a1bd3688bfac66bf5f31baf5
m_adeli <m_adeli@live.com>
2019-03-10 17:38:18 +0330

    admin template

---
+++ admin/incs/sidebar.php
@@ -0,0 +1,40 @@
+<ul class="sidebar navbar-nav">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" role="button" id="post-dropdown" data-toggle="dropdown"
+            aria-expanded="false">
+            <i class="fas fa-fw fa-tachometer-alt"></i>
+            <span>Posts</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="post-dropdown">
+            <a href="#" class="dropdown-item">View All Posts</a>
+            <a href="#" class="dropdown-item">Add Post</a>
+        </div>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-car-alt"></i>
+            <span>Categories</span>
+        </a>
+
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">
+            <i class="fas fa-fw fa-comment-alt"></i>
+            <span>Comments</span>
+        </a>
+    </li>
+    <li class="nav-item " >
+        <a class="nav-link dropdown-toggle" href="#" role="button" id="user-dropdown" aria-expanded="true" data-toggle="dropdown">
+            <i class="fas fa-fw fa-user-alt"></i>
+            <span>Users</span>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="user-dropdown"></div>
+    </li>
+    <li class="nav-item ">
+        <a class="nav-link" href="index.php">
+            <i class="fas fa-fw fa-user-alt"></i>
+            <span>Profile</span>
+        </a>
+    </li>
+
+</ul>
---
+++ admin/index.php
@@ -0,0 +1,30 @@
+<?php include "incs/header.php"?>
+<?php include "incs/navigation.php"?>
+
+
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php include "incs/sidebar.php"?>
+
+    <div id="content-wrapper">
+
+      <div class="container-fluid">
+
+        <!-- Breadcrumbs-->
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item">
+            <a href="#">Dashboard</a>
+          </li>
+          <li class="breadcrumb-item active">Admin</li>
+        </ol>
+
+        <!-- Icon Cards-->
+
+
+      </div>
+      <!-- /.container-fluid -->
+
+      <!-- Sticky Footer -->
+
+ <?php include "incs/footer.php" ?>
